23e529addf625f03cad618da6efbf1f444f92ec9
Kevin Osborne <osborneelectric@users.noreply.github.com>
2018-04-05 18:06:01 +0100

    Update index.js

--- plugins/thirdparty/index.js
+++ plugins/thirdparty/index.js
@@ -15,7 +15,8 @@
       console.log('render');
 
       //add external css
-      $(element).append('<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.8/jquery.jqplot.min.css" />');      
+      $(element).append('<link rel="stylesheet" href="/jqplot/jquery.jqplot.min.css" />');
+                        //http://cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.8/jquery.jqplot.min.css" />');      
 
       //add the chart div to the dom
       var chartDiv = '<div id="' + currentSettings.id + '" style="height:' + currentSettings.chartHeight + 'px;width:' + currentSettings.chartWidth + 'px;"></div>';
